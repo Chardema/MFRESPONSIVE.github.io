4733e41bc3c25b3159948816abf6df06837e9118
chardema <christo59@icloud.com>
2022-03-18 14:40:52 +0100

    premier et dernier

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//ANIMATE SMOOTH SCROLL 
+$('#view-work').on('click',function() {
+    const images = $('#images').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },
+        900
+    )
+})
